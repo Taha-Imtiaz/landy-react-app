5ef2a47a6135738894680cfaa922e6e436283927
ayaz <ayazhussainbs@gmail.com>
2021-03-02 14:04:08 +0500

    Copy to clipboard added

--- src/pages/ProjectDetails/index.js
+++ src/pages/ProjectDetails/index.js
@@ -6,7 +6,12 @@ import Button from "../../components/Button";
 import axios from 'axios';
 import { Modal } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Input from "../../components/Input";
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+
+
 const ProjectDetail = (props) => {
+    const projectUrl = window.location.href
     const history = useHistory();
     // const url = 'https://savings-back.herokuapp.com/api/'
     const url = 'http://localhost:3001/api'
@@ -28,13 +33,46 @@ const ProjectDetail = (props) => {
                 if (res.data.status === 200) {
                     setData(res.data.data)
                 }
-                toast(res.data.message);
-                // setShouldSubmit(true);
             }).catch((e) => {
             });
         loadStripe();
 
     }, [])
+
+    const changeHandler = (e) => {
+        let { name, value } = e.target;
+        setPayment({
+            ...payment,
+            [name]: value,
+        });
+    }
+
+    let validate = () => {
+        if (!payment.number || !payment.exp_month || !payment.exp_year || !payment.cvc || !payment.amount) {
+            return false;
+        }
+        return true;
+    }
+
+    // let handleSubmit = (e) => {
+    //     e.preventDefault();
+    //     if (validate()) {
+    //         axios
+    //             .post(url + '/login', {
+    //                 ...values,
+    //             })
+    //             .then((res) => {
+    //                 if (res.data.status === 200) {
+    //                     localStorage.setItem('saving-token', res.data.token)
+    //                     history.push("/profile")
+    //                 }
+    //                 toast(res.data.message);
+    //             }).catch((e) => {
+    //             });
+    //     } else {
+    //         toast("Please fill the valid values.");
+    //     }
+    // };
 
     const loadStripe = () => {
         if (!window.document.getElementById("stripe-script")) {
@@ -51,29 +89,57 @@ const ProjectDetail = (props) => {
         }
     };
 
-    const contribute = () => {
-        window.Stripe.card.createToken(
-            {
-                number: payment.number,
-                exp_month: payment.exp_month,
-                exp_year: payment.exp_year,
-                cvc: payment.cvc,
-            },
-            (status, response) => {
-                if (status === 200) {
-                    let obj = {
-                        stripeToken: response.id,
-                        amount: payment.amount,
-                        projectId: data._id
-                    };
+    const contribute = (e) => {
+        e.preventDefault();
+        if (validate()) {
+            console.log(payment)
+            window.Stripe.card.createToken(
+                {
+                    number: payment.number,
+                    exp_month: payment.exp_month,
+                    exp_year: payment.exp_year,
+                    cvc: payment.cvc,
+                },
+                (status, response) => {
+                    if (status === 200) {
+                        let obj = {
+                            stripeToken: response.id,
+                            amount: payment.amount,
+                            projectId: data._id
+                        };
+                        axios
+                            .post(url + '/contribute', {
+                                ...obj,
+                            })
+                            .then((res) => {
+                                if (res.data.status === 200) {
+                                    // localStorage.setItem('saving-token', res.data.token)
+                                    // history.push("/profile")
+                                    handleClose();
+                                }
+                                toast(res.data.message);
+                            }).catch((e) => {
+                            });
+                    }
                 }
-            }
-        );
+            );
+        }
     }
+
+    const copyLink = () => {
+
+    }
+
     return (
+
         <Fragment>{data ?
             <div>
                 <h4>Project Detail</h4>
+                <CopyToClipboard text={projectUrl}>
+                    <Button type="button" onClick={() => toast("Copied To Clipboard")}>
+                        Invite
+              </Button>
+                </CopyToClipboard>
                 <p>{data.name}</p>
                 <img src={data.image} />
                 <Button type="button" onClick={handleShow}>
@@ -81,35 +147,33 @@ const ProjectDetail = (props) => {
               </Button>
             </div>
             : null}
-
+            <ToastContainer />
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
-                    <Modal.Title>Modal heading</Modal.Title>
+                    <Modal.Title>Contribute</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <form>
-                        <TextField
+                        <Input
+                            type="number"
                             variant="outlined"
-                            className={style.styleFormFields}
                             required
                             fullWidth
                             size="small"
-                            id="cardno"
+                            id="Card No"
                             label="Card Number"
                             name="number"
                             onChange={changeHandler}
                         />
 
                         <p>Testing Card #: 4242424242424242</p>
-                        <div className={style.currentYear}>
+                        <div>
                             <div>
-                                <TextField
+                                <Input
                                     variant="outlined"
-                                    className={style.styleFormFields}
                                     required
-                                    fullWidth
                                     size="small"
-                                    id="month"
+                                    id="Month"
                                     label="Month"
                                     name="exp_month"
                                     onChange={changeHandler}
@@ -117,13 +181,10 @@ const ProjectDetail = (props) => {
                             </div>
 
                             <div>
-                                <TextField
+                                <Input
                                     variant="outlined"
-                                    className={style.styleFormFields}
-                                    required
-                                    fullWidth
                                     size="small"
-                                    id="year"
+                                    id="Year"
                                     label="Year"
                                     name="exp_year"
                                     onChange={changeHandler}
@@ -131,25 +192,20 @@ const ProjectDetail = (props) => {
                             </div>
                         </div>
 
-                        <TextField
+                        <Input
                             variant="outlined"
-                            className={style.styleFormFields}
-                            required
-                            fullWidth
                             size="small"
-                            id="name"
+                            id="CVC"
                             label="CVC"
                             name="cvc"
                             onChange={changeHandler}
                         />
 
-                        <TextField
+                        <Input
+                            type="number"
                             variant="outlined"
-                            className={style.styleFormFields}
-                            required
-                            fullWidth
                             size="small"
-                            id="amount"
+                            id="Amount"
                             label="Amount"
                             name="amount"
                             onChange={changeHandler}
@@ -157,11 +213,11 @@ const ProjectDetail = (props) => {
                     </form>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
+                    {/* <Button variant="secondary" onClick={handleClose}>
                         Close
-          </Button>
-                    <Button variant="primary" onClick={handleClose}>
-                        Save Changes
+          </Button> */}
+                    <Button variant="primary" onClick={contribute}>
+                        Submit
           </Button>
                 </Modal.Footer>
             </Modal></Fragment>
