0ba59151dfb7079a5205c27a47c34fdd40b7a1c4
ayaz <ayazhussainbs@gmail.com>
2021-03-04 11:54:27 +0500

    Chart added

--- src/components/ProfilePageContainer/index.js
+++ src/components/ProfilePageContainer/index.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ProjectContainer from '../ProjectContainer'
 import SideBar from '../SideBar'
 import * as S from './styles'
+// import { Doughnut } from 'react-chartjs-2';
+import Chart from 'chart.js';
+
 const ProfilePageContainer = (props) => {
     let data = props.data;
+    console.log(data.doughnutChart)
+    useEffect(() => {
+        const ctx = document.getElementById("myChart");
+        new Chart(ctx, {
+            type: "doughnut",
+            data: data.doughnutChart
+        });
+    }, [])
     return (
         <S.ProfilePageContainer>
             <S.ProfilePageArea>
@@ -12,6 +23,7 @@ const ProfilePageContainer = (props) => {
                 </S.SideBar>
                 <S.ProjectArea>
                     <ProjectContainer data={data.user.projects} />
+                    <canvas id="myChart" width="200" height="200"></canvas>
                 </S.ProjectArea>
             </S.ProfilePageArea>
         </S.ProfilePageContainer>
